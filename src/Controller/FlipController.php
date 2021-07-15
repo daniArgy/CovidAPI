3975b671651420c8f645c6c52943086a8ec60118
daniArgy <dgonzalezargibay@gmail.com>
2021-07-15 19:19:24 +0200

    Entrevista tecnica

--- src/Controller/FlipController.php
+++ src/Controller/FlipController.php
@@ -6,17 +6,18 @@ namespace App\Controller;
 
 use App\Entity\FlipType;
 use App\Service\FlipSentence;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-final class FlipController
+final class FlipController extends AbstractController
 {
     public function index(FlipSentence $flipSentence): Response
     {
         $data = [
             'Test 1' => $flipSentence('hola', FlipType::createTypeWord()) === 'aloh',
-            'Test 2' => $flipSentence('hola', FlipType::createTypeSentence()) === 'programacion la de mundo hola',
+            'Test 2' => $flipSentence('hola', FlipType::createTypeSentence()) === 'hola',
             'Test 3' => $flipSentence('cambio de letras', FlipType::createTypeWord()) === 'oibmac ed sartel',
-            'Test 4' => $flipSentence('hola mundo de la programacion', FlipType::createTypeSentence()) === 'aloh',
+            'Test 4' => $flipSentence('hola mundo de la programacion', FlipType::createTypeSentence()) === 'programacion la de mundo hola',
         ];
 
         return new Response($this->printHTML($data));
